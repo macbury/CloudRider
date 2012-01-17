d3184825a607a3fd6b60c1f5178f538cd026cc70
macbury <macbury@gmail.com>
2012-01-17 16:19:35 +0100

    first:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudRider::Application.config.secret_token = '22576084b55b3c5bd8bed8cdcdf0093d877fff0326d7ee9b6bc298721c68a03252c4bd18471cecb49abddeed03dc280d25f7f5dc350724876be2d69328591951'
